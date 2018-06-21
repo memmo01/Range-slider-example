554338f97d0bdcf2571858702ac7ac772c42b182
michael emmons <memmons18@gmail.com>
2018-06-21 18:34:38 -0400

    added ability to record values when slider moves and update alter monthly payment dynamically. Changed style of header

--- index.js
+++ index.js
@@ -4,24 +4,44 @@ $(".slider").on("input",function(e){
  
 	switch(id){
 		case "one":
-		$("#test1").html("$"+value)
+		$("#one").html("$"+value)
+			calculatePayment()
 		break;
 
 		case 'two':
-		$("#test2").html("$"+value)
+		$("#test2").html("$"+value);
+			calculatePayment()
 		break;
 
 		case "three":
 		$("#test3").html("$"+value)
+			calculatePayment()
 		break;
 
 		case "four":
 		$("#test4").html("$"+value)
+			calculatePayment()
 		break;
 
 		case "five":
 		$("#test5").html("$"+value)
+			calculatePayment()
 		break;
 
 		}
 	})
+
+function calculatePayment(){
+
+	let loanAmount= $("#one").val();
+	let tradeIn = $("#two").val();
+	let term = $("#three").val();
+	let interestRate =$("#four").val();
+
+let totalLoan = (loanAmount - tradeIn)
+
+$("#monthly-calculation").html("$"+totalLoan)
+	// let totalLoan = Number(loanAmount) = Number(tradeIn);
+	// console.log(typeof(totalLoan));
+	// console.log(totalLoan) 
+}
